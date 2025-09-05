9bb277fc95ffc06aab229e6e3df9c416d8936e7c
mohammad hosein habibi <mandosein12@gmail.com>
2025-09-05 21:32:12 +0330

    Add files via upload

---
+++ week 6/sentimental-cash/cash.py
@@ -0,0 +1,27 @@
+from cs50 import get_float
+
+while True:
+    A = get_float("Change owed: ")
+
+    if A > 0:
+        break
+    else:
+        continue
+
+cents = round(A * 100)
+
+coin = 0
+
+while cents > 0:
+    if cents >= 25:
+        cents -= 25
+    elif cents >= 10:
+        cents -= 10
+    elif cents >= 5:
+        cents -= 5
+    else:
+        cents -= 1
+
+    coin += 1
+
+print(coin)
